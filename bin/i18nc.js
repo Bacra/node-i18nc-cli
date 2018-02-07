7c0ea6a9b297d8ad5451a8c9c5b8483fc9c7e550
Bacra Woo <bacra.woo@gmail.com>
2018-02-07 23:33:34 +0800

    Add: `code` action

--- bin/i18nc.js
+++ bin/i18nc.js
@@ -7,16 +7,31 @@ var cwd		= process.cwd();
 var program	= require('commander');
 var actions	= require('../lib/main');
 
-program
-	.version(
+program.version(
 		' cli: v'+require('../package.json').version
 		+ '\ncore: v'+require('../').version
-	)
-	.command('code <input> <output>')
-	.option('-d --dbfile [dbfile]', 'dbfile path')
-	.option('-w --output-words [output-words]', 'output translate words')
+	);
+
+
+program.command('code [input] [output]')
+	.description('insert I18N handler to code')
+	.option('-d --dbfile [file]', 'dbfile path')
+	.option('-w --output-word-file [file]', 'output translate words')
+	.option('-o --output-po-dir [dir]', 'output po files dir')
+	.option('-i --input-po-dir [dir]', 'input po files dir')
+	.option('-l --lans [lan1,lan2]', 'pick file languages', function(val)
+		{
+			return val.split(',')
+				.map(function(val){return val.trim()})
+				.filter(function(val){return val});
+		})
+	.option('-n --i18n-handler-name [name]', 'custom I18N handler name')
+	.option('-c', 'only check')
+	.option('-r', 'recurse into directories')
 	.action(function(input, output, args)
 	{
+		if (args.r) input += '/**/*';
+
 		actions.code(cwd, input, output, args)
 			.catch(function(err)
 			{
@@ -25,4 +40,47 @@ program
 	});
 
 
-program.parse(process.argv);
+program.command('help <cmd>')
+	.description('display help for [cmd]')
+	.action(function(cmd)
+	{
+		var self = this;
+
+		var command = findSubCommand(self.parent, cmd);
+		if (!command) throw new Error('No Defined Command, '+cmd);
+
+		// command.help();
+		// remove help info
+		command.outputHelp(function(info)
+		{
+			return info
+				// 删除Options下的help
+				.replace(/ +-h, --help [^\n]+\n/, '')
+				// 在命令行前加上父节点前缀
+				.replace(/Usage: */, 'Usage: '+self.parent.name()+' ')
+				// 如果没有多余的Options，就把这一项干掉
+				.replace(/ +Options:\s+$/, '');
+		});
+	});
+
+
+// find sub command
+function findSubCommand(program, cmd)
+{
+	var command;
+	program.commands.some(function(item)
+	{
+		if (item.name() == cmd || item.alias() == cmd)
+		{
+			command = item;
+			return true;
+		}
+	});
+
+	return command;
+}
+
+
+var argv = process.argv.slice();
+if (argv.length < 3) argv.push('--help');
+program.parse(argv);
