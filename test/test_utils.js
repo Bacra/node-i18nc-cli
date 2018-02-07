36eea344b22eb93e26ef23844809a30fd2557626
Bacra Woo <bacra.woo@gmail.com>
2018-02-07 11:32:39 +0800

    Add:  isI18NHandlerAllWrap

--- test/test_utils.js
+++ test/test_utils.js
@@ -7,32 +7,97 @@ var TMP_PATH      = __dirname+'/tmp/';
 
 describe('#utils', function()
 {
-	before(function()
+	describe('#mulitResult2POFiles', function()
 	{
-		return Promise.all(
-			[
-				mkdirp(TMP_PATH+'mulitResult2POFiles')
-			]);
+		before(function()
+		{
+			return mkdirp(TMP_PATH+'mulitResult2POFiles');
+		});
+
+		it('#base', function()
+		{
+			var mulitI18NCOutput = require('./input/mulit_i18nc_output');
+			return i18nc.util.mulitResult2POFiles(mulitI18NCOutput, TMP_PATH+'mulitResult2POFiles',
+				{
+					pickFileLanguages: ['zh-TW', 'en-US']
+				});
+		});
 	});
 
-	it('#mulitResult2POFiles', function()
+	describe('#loadPOFiles', function()
 	{
-		var mulitI18NCOutput = require('./input/mulit_i18nc_output');
-		return i18nc.util.mulitResult2POFiles(mulitI18NCOutput, TMP_PATH+'mulitResult2POFiles',
-			{
-				pickFileLanguages: ['zh-TW', 'en-US']
-			});
+		it('#base', function()
+		{
+			return i18nc.util.loadPOFiles(__dirname+'/input/pofiles')
+				.then(function(json)
+				{
+					var otherJson = autoTestUtils.requireAfterWrite('loadpofiles_output.json', json);
+					expect(json).to.eql(otherJson);
+				});
+		});
 	});
 
-	it('#loadPOFiles', function()
+	describe('#isI18NHandlerAllWrap', function()
 	{
-		return i18nc.util.loadPOFiles(__dirname+'/input/pofiles')
-			.then(function(json)
-			{
-				var otherJson = autoTestUtils.requireAfterWrite('loadpofiles_output.json', json);
-				expect(json).to.eql(otherJson);
-			});
-	});
+		var arr1 = {DEFAULTS:['中文1', '中文2']};
+		var arr11 = {DEFAULTS:['中文2', '中文1']};
+		var arr2 = {DEFAULTS:['中文2']};
+		var arr3 = {DEFAULTS:['中文3']};
 
-	it('#isI18NHandlerAllWrap', function(){});
+		it('#base', function()
+		{
+			expect(i18nc.util.isI18NHandlerAllWrap({})).to.be(true);
+			expect(i18nc.util.isI18NHandlerAllWrap(
+				{
+					codeTranslateWords: arr1
+				}))
+				.to.be(false);
+			expect(i18nc.util.isI18NHandlerAllWrap(
+				{
+					codeTranslateWords: arr1,
+					I18NArgsTranslateWords: arr11
+				}))
+				.to.be(true);
+			expect(i18nc.util.isI18NHandlerAllWrap(
+				{
+					codeTranslateWords: arr1,
+					I18NArgsTranslateWords: arr2
+				}))
+				.to.be(false);
+			expect(i18nc.util.isI18NHandlerAllWrap(
+				{
+					codeTranslateWords: arr1,
+					I18NArgsTranslateWords: arr3
+				}))
+				.to.be(false);
+		});
+
+		it('#subScopeData', function()
+		{
+			expect(i18nc.util.isI18NHandlerAllWrap({subScopeDatas:[]})).to.be(true);
+			expect(i18nc.util.isI18NHandlerAllWrap({subScopeDatas:[{codeTranslateWords:arr1}]})).to.be(false);
+			expect(i18nc.util.isI18NHandlerAllWrap(
+				{
+					subScopeDatas:[
+						{
+							codeTranslateWords:arr1,
+							I18NArgsTranslateWords:arr11
+						}]
+				}))
+				.to.be(true);
+			expect(i18nc.util.isI18NHandlerAllWrap(
+				{
+					subScopeDatas:[
+						{
+							codeTranslateWords:arr1,
+							I18NArgsTranslateWords:arr11
+						},
+						{
+							codeTranslateWords: arr1,
+							I18NArgsTranslateWords: arr2
+						}]
+				}))
+				.to.be(false);
+		});
+	});
 });
