eb4a6aea534a317bfc3c5f45fcfe21b72a819c0d
Bacra Woo <bacra.woo@gmail.com>
2018-07-08 20:25:11 +0800

    Clear: requireAfterWrite

--- test/auto_test_utils.js
+++ test/auto_test_utils.js
@@ -5,10 +5,10 @@ var fs = Promise.promisifyAll(require('fs'));
 var expect = require('expect.js');
 
 
-exports.requireAfterWrite = function requireAfterWrite(filename, data, options)
+exports.requireAfterWrite = function requireAfterWrite(filename, data)
 {
 	var file = __dirname+'/output/'+filename;
-	if (!process.env.TEST_BUILD) return _requireOrFs(file, options);
+	if (!process.env.TEST_BUILD) return require(file);
 
 	if (typeof data == 'object')
 	{
@@ -17,23 +17,8 @@ exports.requireAfterWrite = function requireAfterWrite(filename, data, options)
 
 	fs.writeFileSync(file, data);
 
-	return _requireOrFs(file, options);
+	return require(file);
 }
-
-function _requireOrFs(file, options)
-{
-	options || (options = {});
-
-	switch(options.readMode)
-	{
-		case 'string':
-			return fs.readFileSync(file, {encoding: 'utf8'});
-
-		default:
-			return require(file);
-	}
-}
-
 
 exports.code2arr = function code2arr(code)
 {
