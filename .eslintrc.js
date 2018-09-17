767bf0e0ae00cae4fd7d1842eb517077592a81b7
Bacra <bacra.woo@gmail.com>
2018-09-17 23:04:52 +0800

    Update: eslint for async await

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports =
 {
-	extends: 'eslint-config-brcjs'
+	parser: 'babel-eslint',
+	extends: 'eslint-config-brcjs',
+	env: {es6: true},
+	rules:
+	{
+		'generator-star-spacing': 0,	// allow async-await
+		'no-const-assign': 2,			//禁止修改const声明的变量
+	},
 };
