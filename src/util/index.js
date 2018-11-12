5f75675c3ac4e4c8634a8e8960ae3b6eba306b46
Bacra <bacra.woo@gmail.com>
2018-11-13 02:13:05 +0800

    Update: export scan in util.cli

--- src/util/index.js
+++ src/util/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const _ = require('lodash');
+const cli = exports.cli = require('./cli_printer');
+cli.scan = require('./cli_scan');
 
 exports.refs = require('i18nc-po/lib/refs_utils');
-exports.cli = require('./cli_printer');
 exports.opt =
 {
 	tpl: require('../../global/lib/getlans_opt.tpl.js')
